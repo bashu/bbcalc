38d6548f616c22857031952b2de28bfa1fad558e
Basil Shubin <basil.shubin@gmail.com>
2007-11-04 07:57:12 +0000

    New calculator: ideal body measurements

---
+++ lib/gui/calculators/idealbody.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+""" Ideal Body Measurements calculator """
+
+from gettext import gettext as _
+from lib.utils import *
+from lib.utils.unitconvertor import in2cm, cm2in
+
+WRISTCOEF = [6.5, 0.85, 0.70, 0.53, 0.37, 0.36, 0.34, 0.29]
+
+
+class IdealBody:
+
+    def __init__(self, parent):
+        self.parent = parent
+
+        # Get a 'table' with calculator from the XML file
+        self.calc_table = self.parent.xml.get_widget('ideal_body_table')
+        self.old_parent = self.calc_table.get_parent()
+
+        # Get several widgets, that will be used later
+        self.unit_combobox = self.parent.xml.get_widget('unit_combobox2')
+        self.wrist_spinbutton = self.parent.xml.get_widget('wrist_spinbutton1')
+
+        self.chest_entry = self.parent.xml.get_widget('chest_entry1')
+        self.waist_entry = self.parent.xml.get_widget('waist_entry1')
+        self.hip_entry = self.parent.xml.get_widget('hip_entry1')
+        self.bicep_entry = self.parent.xml.get_widget('bicep_entry1')
+        self.forearm_entry = self.parent.xml.get_widget('forearm_entry1')
+        self.thigh_entry = self.parent.xml.get_widget('thigh_entry1')
+        self.calve_entry = self.parent.xml.get_widget('calve_entry1')
+        self.neck_entry = self.parent.xml.get_widget('neck_entry1')
+
+        self.results = [self.wrist_spinbutton, self.chest_entry, self.hip_entry,
+                        self.waist_entry, self.thigh_entry, self.neck_entry,
+                        self.bicep_entry, self.calve_entry, self.forearm_entry]
+        
+        # Set active item for unit selection box
+        self.unit = 1
+        self.unit_combobox.set_active(self.unit)
+        
+        # Connect event handlers
+        signals = {'on_unit_combobox2_changed' : self.on_unit_combobox2_changed,
+                   'on_ideal_body_calc' : self.on_ideal_body_calc}
+        self.parent.xml.signal_autoconnect(signals)
+
+    def on_ideal_body_calc(self, *args):
+        wrist = self.wrist_spinbutton.get_value()    
+        array = [wrist]
+        array.append(array[0] * WRISTCOEF[0])
+        for idx in xrange(1, 8):
+            array.append(array[1] * WRISTCOEF[idx])
+
+        for idx in xrange(0, 9):
+            array[idx] = round((array[idx] + 0.0001) * 100, 2)
+            array[idx] = round(array[idx] / 100.0, 2)
+
+        for idx in xrange(1, 9):
+            self.results[idx].set_text(str(array[idx]))
+
+    def on_unit_combobox2_changed(self, *args):
+        """Handle unit conversion"""
+        if self.unit != self.unit_combobox.get_active():
+            self.unit = self.unit_combobox.get_active()
+            wrist = self.wrist_spinbutton.get_value()
+            # Perform conversion for the weight value
+            if self.unit == CENTIMETERS:
+                self.wrist_spinbutton.set_value(in2cm(wrist, 2))
+            else:
+                self.wrist_spinbutton.set_value(cm2in(wrist, 2))
+
+
