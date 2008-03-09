bf10c49c77b316e604f95d858e21dc0d6d573839
Basil Shubin <basil.shubin@gmail.com>
2008-03-09 14:10:17 +0000

    Updating pversion and pyear

--- lib/version.py
+++ lib/version.py
@@ -3,9 +3,9 @@
 from gettext import gettext as _
 
 pname        = "BBCalc"
-pversion     = "0.7"
+pversion     = "0.8"
 pauthor      = "Basil Shubin <bashu@users.sourceforge.net>"
-pyear        = "2005-2007"
+pyear        = "2005-2008"
 pwebsite     = "http://bbcalc.sf.net/"
 pdescription = _("""BBCalc (Body Building Calculators) is a set of calculators
 related to body building and fitness topics.""")
