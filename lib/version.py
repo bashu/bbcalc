b9191a9fb446125f602af12dfacec4073623f2e5
Basil Shubin <basil.shubin@gmail.com>
2009-01-17 09:45:13 +0000

    Updating year

--- lib/version.py
+++ lib/version.py
@@ -9,7 +9,7 @@ from gettext import gettext as _
 pname        = "BBCalc"
 pversion     = "0.8"
 pauthor      = "Basil Shubin <bashu@users.sourceforge.net>"
-pyear        = "2005-2008"
+pyear        = "2005-2009"
 pwebsite     = "http://bbcalc.sf.net/"
 pdescription = _("""BBCalc (Body Building Calculators) is a set of calculators
 related to body building and fitness topics.""")
