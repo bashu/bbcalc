1b45aeb65262ce992d0e9f570c953aa4a1f4677a
Basil Shubin <basil.shubin@gmail.com>
2009-02-09 05:41:32 +0000

    Refactoring the code

--- lib/gui/calculators/idealbody.py
+++ lib/gui/calculators/idealbody.py
@@ -15,18 +15,17 @@ import gconf
 from gettext import gettext as _
 
 from lib.gui.glade import Component
+from lib.gui.calculators.calculator import Calculator
+
+from lib.calculators.idealbody import ideal_body_calc
 
 from lib import GCONF_CLIENT, GCONF_MEASUREMENT_SYSTEM, DEFAULT_MEASUREMENT_SYSTEM, GCONF_SYSTEM_IMPERIAL
 from lib.utils import METRIC, IMPERIAL
-from lib.utils.unitconvertor import in2cm, cm2in
-
-# General constants
-WRISTCOEF = [6.5, 0.85, 0.70, 0.53, 0.37, 0.36, 0.34, 0.29]
 
 
-class IdealBody(Component):
-    
-    description = _("Ideal Body Measurements Calculator")
+class IdealBody(Component, Calculator):
+
+    description = _(u'Ideal Body Measurements Calculator')
     unit = None
 
     def __init__(self):
@@ -36,39 +35,38 @@ class IdealBody(Component):
         self.results = [self.wrist_spinbutton, self.chest_entry, self.hip_entry,
                         self.waist_entry, self.thigh_entry, self.neck_entry,
                         self.bicep_entry, self.calve_entry, self.forearm_entry]
-        
+
+        self.length_widgets = {self.unit_combobox.name :
+                               [self.wrist_spinbutton]}
+        self.length_units = {self.unit_combobox.name : self.unit}
+
+        # Loading default values from GConf
+        self.load_gconf_defaults()
+        # Creating GConf notification handlers
+        self.create_gconf_notification()
+
+    def load_gconf_defaults(self):
+        """Load GConf defaults"""
         # Set active item for unit selection box
         if DEFAULT_MEASUREMENT_SYSTEM == GCONF_SYSTEM_IMPERIAL:
             self.unit_combobox.set_active(IMPERIAL)
         else:
             self.unit_combobox.set_active(METRIC)   
 
+    def create_gconf_notification(self):
+        """Bind GConf notification handlers"""
         self.unit_notify = GCONF_CLIENT.notify_add(GCONF_MEASUREMENT_SYSTEM, \
             lambda x, y, z, a: self.on_unit_combobox_changed(z.value))
 
     def on_ideal_body_calc(self, *args):
-        # Unit conversion
-        if self.unit != self.unit_combobox.get_active():
-            self.unit = self.unit_combobox.get_active()
-            wrist = self.wrist_spinbutton.get_value()
-            # Perform conversion for the wrist value
-            if self.unit == METRIC:
-                self.wrist_spinbutton.set_value(in2cm(wrist, 2))
-            else:
-                self.wrist_spinbutton.set_value(cm2in(wrist, 2))
-        # Results calculation
-        wrist = self.wrist_spinbutton.get_value()    
-        array = [wrist]
-        array.append(array[0] * WRISTCOEF[0])
-        for idx in xrange(1, 8):
-            array.append(array[1] * WRISTCOEF[idx])
+        # Perform unit conversion if needed
+        self.unit_conversion_length(self.unit_combobox.name, 
+                                    self.unit_combobox.get_active())
 
-        for idx in xrange(0, 9):
-            array[idx] = round((array[idx] + 0.0001) * 100, 2)
-            array[idx] = round(array[idx] / 100.0, 2)
+        results_array = ideal_body_calc(self.wrist_spinbutton.get_value())
 
         for idx in xrange(1, 9):
-            self.results[idx].set_text(str(array[idx]))
+            self.results[idx].set_text(str(results_array[idx]))
 
     def on_unit_combobox_changed(self, value):
         """Handle unit conversion"""
--- lib/gui/calculators/onerepmax.py
+++ lib/gui/calculators/onerepmax.py
@@ -15,56 +15,54 @@ import gconf
 from gettext import gettext as _
 
 from lib.gui.glade import Component
+from lib.gui.calculators.calculator import Calculator
+
+from lib.calculators.onerepmax import onerep_max_calc
 
 from lib import GCONF_CLIENT, GCONF_MEASUREMENT_SYSTEM, DEFAULT_MEASUREMENT_SYSTEM, GCONF_SYSTEM_IMPERIAL
 from lib.utils import METRIC, IMPERIAL
-from lib.utils.unitconvertor import kg2lb, lb2kg
-
-# General constants
-REPCOEF = [0, 1, 0.955, 0.917, 0.885, 0.857, 0.832, 0.809, 
-           0.788, 0.769, 0.752, 0.736, 0.721, 0.706, 0.692, 0.678]
 
 
-class OneRepMax(Component):
-    
-    description = _("One-Rep Max Calculator")
+class OneRepMax(Component, Calculator):
+
+    description = _(u'One-Rep Max Calculator')
     unit = None
-    
+
     def __init__(self):
         Component.__init__(self, GLADE_FILE, 'one_rep_max_table')
 
+        self.mass_widgets = {self.unit_combobox.name : [self.weight_spinbutton]}
+        self.mass_labels = {self.unit_combobox.name : [self.result_label]}
+        self.mass_units = {self.unit_combobox.name : self.unit}
+
+        # Loading default values from GConf
+        self.load_gconf_defaults()
+        # Creating GConf notification handlers
+        self.create_gconf_notification()
+
+    def load_gconf_defaults(self):
+        """Load GConf defaults"""
         # Set active item for unit selection box
         if DEFAULT_MEASUREMENT_SYSTEM == GCONF_SYSTEM_IMPERIAL:
             self.unit_combobox.set_active(IMPERIAL)
         else:
             self.unit_combobox.set_active(METRIC)
-        
+
+    def create_gconf_notification(self):
+        """Bind GConf notification handlers"""
         self.unit_notify = GCONF_CLIENT.notify_add(GCONF_MEASUREMENT_SYSTEM, \
             lambda x, y, z, a: self.on_unit_combobox_changed(z.value))
 
     def on_onerep_max_calc(self, *args):
-        """
-        This formula comes from 'A Practical Approach to Strength Training' by
-        Matt Brzycki
-        """
-        # Unit conversion
-        if self.unit != self.unit_combobox.get_active():
-            self.unit = self.unit_combobox.get_active()
-            weight = self.weight_spinbutton.get_value()
-            # Perform conversion for the weight value
-            if self.unit == METRIC:
-                self.weight_spinbutton.set_value(lb2kg(weight, 2))
-            else:
-                self.weight_spinbutton.set_value(kg2lb(weight, 2))
-            # Change label for the resulting unit type
-            if self.unit_combobox.get_active() != METRIC:
-                self.result_label.set_text(_('lbs'))
-            else:
-                self.result_label.set_text(_('kg'))
+        # Perform unit conversion if needed
+        self.unit_conversion_mass(self.unit_combobox.name, 
+                                  self.unit_combobox.get_active())
+
         # Result calculation
         weight = self.weight_spinbutton.get_value()
-        max_single = weight / REPCOEF[int(self.reps_spinbutton.get_value())]
-        self.result_entry.set_text(str(round(max_single, 2)))
+        reps = self.reps_spinbutton.get_value()
+
+        self.result_entry.set_text(str(onerep_max_calc(weight, int(reps))))
 
     def on_unit_combobox_changed(self, value):
         if value is None or value.type != gconf.VALUE_STRING:
