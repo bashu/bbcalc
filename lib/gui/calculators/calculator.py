c7e4ef77e00b3dc54c942776c246b443f94eb11b
Basil Shubin <basil.shubin@gmail.com>
2009-02-20 09:30:42 +0000

    New method: distance conversion

--- lib/gui/calculators/calculator.py
+++ lib/gui/calculators/calculator.py
@@ -6,11 +6,12 @@ Generic class for all calculators
 $Id$
 """
 
-from lib.utils import METRIC, STR_KG, STR_LBS, STR_CM, STR_IN
-from lib.utils.unitconvertor import kg2lb, lb2kg, in2cm, cm2in
+from lib.utils import METRIC, STR_KG, STR_LBS, STR_CM, STR_IN, STR_KM, STR_MI
+from lib.utils.unitconvertor import kg2lb, lb2kg, in2cm, cm2in, km2mi, mi2km
 
 
 class Calculator:
+    # TODO: Refactore this class
     
     mass_widgets = {}
     mass_labels = {}
@@ -19,6 +20,10 @@ class Calculator:
     length_widgets = {}
     length_labels = {}
     length_units = {}
+    
+    distance_widgets = {}
+    distance_labels = {}
+    distance_units = {}
     
     def unit_conversion_mass(self, key, value):
         """Unit conversion for mass widgets"""
@@ -71,5 +76,32 @@ class Calculator:
                     label.set_text(STR_IN)
                 else: 
                     label.set_text(STR_CM)
+        except:
+            pass
+
+    def unit_conversion_distance(self, key, value):
+        """Unit conversion for distance widgets"""
+        try:
+            if self.distance_units[key] == value:
+                return False
+        except:
+            return False
+        self.distance_units[key] = value
+        try:
+            for widget in self.distance_widgets[key]:
+                # Perform conversion for the distance value
+                if self.distance_units[key] == METRIC: 
+                    widget.set_value(mi2km(widget.get_value()))
+                else:
+                    widget.set_value(km2mi(widget.get_value()))
+        except:
+            pass
+        try:
+            for label in self.distance_labels[key]:
+                # Change label for the resulting unit type
+                if self.distance_units[key] != METRIC: 
+                    label.set_text(STR_MI)
+                else:
+                    label.set_text(STR_KM)
         except:
             pass
