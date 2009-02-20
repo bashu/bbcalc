77e972a4345dfafec86316173ba93e6349b8361a
Basil Shubin <basil.shubin@gmail.com>
2009-02-20 09:27:43 +0000

    New calculator: Running (Calorie Burned) calculator

---
+++ lib/calculators/running.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Calorie Burning: Running calculator
+
+$Id$
+"""
+
+from lib.utils import IMPERIAL, METRIC
+
+from lib.utils.unitconvertor import km2mi, kg2lb
+
+
+def running_calc(weight, distance, weight_unit=METRIC, distance_unit=METRIC, precision=2):
+    """Calorie Burning: Running calculator"""
+    if weight_unit != IMPERIAL:
+        weight = kg2lb(float(weight))
+    if distance_unit != IMPERIAL:
+        distance = km2mi(float(distance))
+
+    # Formulae: calories burned = (distance * weight) * 0.653
+    difference = round(((distance * weight) * 0.653), precision)          
+
+    return difference
