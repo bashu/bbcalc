5ae50adaf38a200a0fa1ec973d2784dfe3f9614a
Basil Shubin <basil.shubin@gmail.com>
2009-02-20 09:33:55 +0000

    New convertors: kilometers to miles; miles to kilometers

--- lib/utils/unitconvertor.py
+++ lib/utils/unitconvertor.py
@@ -6,7 +6,7 @@ Miscellaneous unit converters
 $Id$
 """
 
-from lib.utils import POUNDS, INCHES
+from lib.utils import POUNDS, INCHES, MILES
 
 def kg2lb(kg, precision=2):
     """Convert kilogramms to pounds"""
@@ -23,3 +23,11 @@ def cm2in(cm, precision=2):
 def in2cm(inches, precision=2):
     """Convert inches to centimeters"""
     return round(inches * INCHES, precision)
+
+def km2mi(km, precision=2):
+    """Convert kilometers to miles"""
+    return round(km / MILES, precision)
+
+def mi2km(miles, precision=2):
+    """Convert miles to kilometers"""
+    return round(miles * MILES, precision)
