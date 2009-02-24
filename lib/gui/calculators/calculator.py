7e31061c5dc1c5a8e7a8b42097d9c851d9f87a2e
Basil Shubin <basil.shubin@gmail.com>
2009-02-24 06:03:55 +0000

    Code refactoring

--- lib/gui/calculators/calculator.py
+++ lib/gui/calculators/calculator.py
@@ -11,8 +11,7 @@ from lib.utils.unitconvertor import kg2lb, lb2kg, in2cm, cm2in, km2mi, mi2km
 
 
 class Calculator:
-    # TODO: Refactore this class
-    
+
     mass_widgets = {}
     mass_labels = {}
     mass_units = {}
@@ -24,84 +23,56 @@ class Calculator:
     distance_widgets = {}
     distance_labels = {}
     distance_units = {}
-    
-    def unit_conversion_mass(self, key, value):
-        """Unit conversion for mass widgets"""
+
+    def unit_conversion(self, widgets, labels, units, metric, imperial, metric_str, imperial_str, key, value):
+        """Generic method for unit conversion
+        
+        widgets - dictionary of widgets,
+        labels - dictionary of labels,
+        units - dictionary of units,
+        metric - function for imperial to metric conversion,
+        imperial - function from metric to imperial conversion,
+        metric_str - unicode string for metric,
+        imperial_str - unicode string for imperial,
+        key - unique key for widgets, labels and units,
+        value - new value
+        """
         try:
-            if self.mass_units[key] == value:
+            if units[key] == value:
                 return False
         except:
             return False
-        self.mass_units[key] = value
+        units[key] = value
         try:
-            for widget in self.mass_widgets[key]:
+            for widget in widgets[key]:
                 # Perform conversion for the mass value
-                if self.mass_units[key] == METRIC: 
-                    widget.set_value(lb2kg(widget.get_value()))
+                if units[key] == METRIC: 
+                    widget.set_value(metric(widget.get_value()))
                 else:
-                    widget.set_value(kg2lb(widget.get_value()))
+                    widget.set_value(imperial(widget.get_value()))
         except:
             pass
         try:
-            for label in self.mass_labels[key]:
+            for label in labels[key]:
                 # Change label for the resulting unit type
-                if self.mass_units[key] != METRIC: 
-                    label.set_text(STR_LBS)
+                if units[key] != METRIC: 
+                    label.set_text(imperial_str)
                 else:
-                    label.set_text(STR_KG)
+                    label.set_text(metric_str)
         except:
             pass
+
+    def unit_conversion_mass(self, key, value):
+        """Unit conversion for mass widgets"""
+        self.unit_conversion(self.mass_widgets, self.mass_labels, self.mass_units, 
+                             lb2kg, kg2lb, STR_KG, STR_LBS, key, value)
 
     def unit_conversion_length(self, key, value):
         """Unit conversion for length widgets"""
-        try:
-            if self.length_units[key] == value:
-                return False
-        except:
-            return False
-        self.length_units[key] = value
-        try:
-            for widget in self.length_widgets[key]:
-                # Perform conversion for the length value
-                if self.length_units[key] == METRIC:
-                    widget.set_value(in2cm(widget.get_value()))
-                else:
-                    widget.set_value(cm2in(widget.get_value()))
-        except:
-            pass
-        try:
-            for label in self.length_labels[key]:
-                # Change label for the resulting unit type
-                if self.length_units[key] != METRIC: 
-                    label.set_text(STR_IN)
-                else: 
-                    label.set_text(STR_CM)
-        except:
-            pass
+        self.unit_conversion(self.length_widgets, self.length_labels, self.length_units, 
+                             in2cm, cm2in, STR_CM, STR_IN, key, value)
 
     def unit_conversion_distance(self, key, value):
         """Unit conversion for distance widgets"""
-        try:
-            if self.distance_units[key] == value:
-                return False
-        except:
-            return False
-        self.distance_units[key] = value
-        try:
-            for widget in self.distance_widgets[key]:
-                # Perform conversion for the distance value
-                if self.distance_units[key] == METRIC: 
-                    widget.set_value(mi2km(widget.get_value()))
-                else:
-                    widget.set_value(km2mi(widget.get_value()))
-        except:
-            pass
-        try:
-            for label in self.distance_labels[key]:
-                # Change label for the resulting unit type
-                if self.distance_units[key] != METRIC: 
-                    label.set_text(STR_MI)
-                else:
-                    label.set_text(STR_KM)
-        except:
-            pass
+        self.unit_conversion(self.distance_widgets, self.distance_labels, self.distance_units, 
+                             mi2km, km2mi, STR_KM, STR_MI, key, value)
