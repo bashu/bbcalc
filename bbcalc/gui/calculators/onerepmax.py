7c7c212d5a3bc6e9ca947debff5e781b1f672706
Basil Shubin <basil.shubin@gmail.com>
2009-08-24 05:33:08 +0000

    Ported to Kiwi framework

--- bbcalc/gui/calculators/onerepmax.py
+++ bbcalc/gui/calculators/onerepmax.py
@@ -1,70 +1,56 @@
-## -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-import gconf
-
-from kiwi.ui.delegates import SlaveDelegate
-
-import bbcalc.utils.config as config
-
+from bbcalc.gui.calculators.baseclass import BaseModel, BaseClass
 from bbcalc.calculators.onerepmax import onerep_max_calc
 
-from bbcalc.utils import METRIC, IMPERIAL
 from bbcalc.utils.unitconvertor import convert_mass
 
 # Predefined values, used somewhere else. [0] - Imperial, [1] - Metric
 DEFAULT_WEIGHT = (220.0, 100.0)
 
 
-class OneRepMax(SlaveDelegate):
+class CustomModel(BaseModel):
+
+    weight_spinbutton = float()
+    reps_spinbutton = 8
+    unit_combobox = int()
+    result_entry = float()
+    result_label = unicode()
+
+    def set_unit_combobox(self, value):
+        self.weight_spinbutton = convert_mass(self.weight_spinbutton, value)
+        self.unit_combobox = value
+
+    def get_result_entry(self):
+        return onerep_max_calc(self.weight_spinbutton, self.reps_spinbutton)
+
+    def get_result_label(self):
+        return self.weight_abbr[self.unit_combobox]
+
+
+class OneRepMax(BaseClass):
+
     gladefile = 'one_rep_max_window'
+    model = CustomModel()
+    widgets = ('result_entry', 'result_label', 'weight_spinbutton', )
+    proxy_widgets = ('result_entry', 'result_label', 'unit_combobox', 'weight_spinbutton', 'reps_spinbutton', )
+    measurement_system_widgets = ('unit_combobox', )
 
     def __init__(self):
-        SlaveDelegate.__init__(self)
+        BaseClass.__init__(self)
 
-        # Loading default values from GConf
-        self.load_gconf_defaults()
-        # Creating GConf notification handlers
-        self.create_gconf_notification()
-
-    def load_gconf_defaults(self):
-        """Load GConf defaults"""
-        self.config = config.Config()
-        # Set active item for unit selection box
-        self.unit_combobox.set_active(self.config.measurement_system)
-        # Set default value(s)
-        self.weight_spinbutton.set_value(DEFAULT_WEIGHT[self.config.measurement_system])
-
-    def create_gconf_notification(self):
-        """Bind GConf notification handlers"""
-        self.config.notify_add(config.GCONF_MEASUREMENT_SYSTEM, self.on_gconf_changed)
+    def _setup_widgets(self):
+        self.unit_combobox.prefill([(value, key)
+                for key, value in self.model.weight_types.items()])
+        self.model.weight_spinbutton = DEFAULT_WEIGHT[self.config.measurement_system]
 
     # Signal handlers
 
     def after_unit_combobox__changed(self, entry, *args):
-        active = entry.get_active()
-        weight = self.weight_spinbutton.get_value()
-        self.weight_spinbutton.set_value(convert_mass(weight, active))
+        self.proxy.update_many((self.widgets))
 
-    # TODO: Re-factore this methods
     def after_weight_spinbutton__changed(self, entry, *args):
-        # Result calculation
-        weight = self.weight_spinbutton.get_value()
-        reps = self.reps_spinbutton.get_value()
-
-        self.result_entry.set_text(str(onerep_max_calc(weight, int(reps))))
+        self.proxy.update('result_entry')
 
     def after_reps_spinbutton__changed(self, entry, *args):
-        # Result calculation
-        weight = self.weight_spinbutton.get_value()
-        reps = self.reps_spinbutton.get_value()
-
-        self.result_entry.set_text(str(onerep_max_calc(weight, int(reps))))
-
-    def on_gconf_changed(self, value):
-        """Handle unit conversion"""
-        if value is None or value.type != gconf.VALUE_STRING:
-            return
-        if value.get_string() == config.GCONF_SYSTEM_IMPERIAL:
-            self.unit_combobox.set_active(IMPERIAL)
-        if value.get_string() == config.GCONF_SYSTEM_METRIC:
-            self.unit_combobox.set_active(METRIC)
+        self.proxy.update('result_entry')
