3ce45f8ba8d79b92c1397db729e363530cbe6b12
Basil Shubin <basil.shubin@gmail.com>
2009-08-26 06:00:28 +0000

    Removing useless code

--- bbcalc/utils/__init__.py
+++ bbcalc/utils/__init__.py
@@ -11,16 +11,6 @@ POUNDS = 2.2
 KILOMETERS = 1
 MILES = 1.61
 
-# Translatable unicode strings
-STR_KG = _(u'kg')
-STR_LBS = _(u'lbs')
-
-STR_CM = _(u'cm')
-STR_IN = _(u'in')
-
-STR_KM = _(u'km')
-STR_MI = _(u'mi')
-
 MALE = 0
 FEMALE = 1
 
